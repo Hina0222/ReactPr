8d2fd00408756ffe9a0f17519a52a07bd746a0d3
Hina0222 <hina0222@naver.com>
2024-05-26 00:05:23 +0900

    redux-toolkit 새로운 값 생성 후 상태 변화 시켜기

--- study/src/components/Counter.jsx
+++ study/src/components/Counter.jsx
@@ -1,11 +1,12 @@
 import { INCREMENT, counterActions } from '../store';
 //import classes from './Counter.module.css';
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Counter = () => {
     const dispatch = useDispatch();//액션을 도와주게하는 훅
     const counter = useSelector(state => state.counter)//전역상태를 갖고오기
     const show = useSelector(state => state.showCounter)
+    const showBool = useSelector(state => state.showBool)
     const incrementHandler = () => {
         dispatch(counterActions.increment());
     }
@@ -21,16 +22,18 @@ const Counter = () => {
     };
 
     return (
-    <main >
-        <h1>Redux Counter</h1>
-        {show &&<div >{counter}</div>}
-        <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={increaseHandler}>Increment +5</button>
-        <button onClick={decrementHandler}>Decrement</button>
-        </div>
-        <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
+        <main >
+            <h1>Redux Counter</h1>
+            {show && <div >{counter}</div>}
+            {!show && <div> 어쩔테레비 </div>}
+            <div>{showBool}</div>
+            <div>
+                <button onClick={incrementHandler}>Increment</button>
+                <button onClick={increaseHandler}>Increment +5</button>
+                <button onClick={decrementHandler}>Decrement</button>
+            </div>
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
     );
 };
 
--- study/src/store/index.js
+++ study/src/store/index.js
@@ -1,31 +1,33 @@
-import {createStore} from 'redux';
-import {createSlice,configureStore} from '@reduxjs/toolkit';
+/*eslint-disable*/
+import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = {counter:0,showCounter:true}//초기값 설정
+const initialState = { counter: 0, showCounter: true, showBool: "True" }//초기값 설정
 const counterSlice = createSlice({
-    name:'counter',//리듀서 이름
-    initialState:initialState,//초기값 할당
-    reducers:{
-        increment(state){//액션
+    name: 'counter',//리듀서 이름
+    initialState: initialState,//초기값 할당
+    reducers: {
+        increment(state) {//액션
             //원래 리덕스의 상태를 이용하려면 불변성을 이용하여
             // 객체를 복사후 값을 증가시켰지만
-            // 자동으로 리덕스 툴킷은 객체를 자동으로 복사를 해준다.
+            // 리덕스 툴킷은 객체를 자동으로 복사를 해준다.
             state.counter++
         },
-        decrement(state){//액션
+        decrement(state) {//액션
             state.counter--
         },
-        increase(state,action){//액션
-            state.counter = state.counter + action.payload      
+        increase(state, action) {//액션
+            state.counter = state.counter + action.payload
         },
-        toggleCounter(state){//액션
+        toggleCounter(state) {//액션
             state.showCounter = !state.showCounter
+            state.showBool = state.showCounter ? "True" : "False"
         },
     }
 })
 
 const store = configureStore({//병합해서 store에 저장가능
-    reducer:counterSlice.reducer
+    reducer: counterSlice.reducer
 })
 
 export const counterActions = counterSlice.actions

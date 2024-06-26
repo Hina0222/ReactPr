e4e43adcb3ae4bf3dc53e54ade399482b0c4ad04
Hina0222 <hina0222@naver.com>
2024-06-26 23:36:31 +0900

    useState,useEffect 동작 순서

--- test1/src/useState_ex/UseState1.jsx
+++ test1/src/useState_ex/UseState1.jsx
@@ -1,23 +1,57 @@
 /* eslint-disable*/
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const UseState1 = () => {
-    const [time, setTime] = useState(1);
+    // const [count, setCount] = useState(0);
+    // console.log("State", count)
 
-    const handleClick = () => {
-        let newTime;
-        if (time >= 12) {
-            newTime = 1;
-        } else {
-            newTime = time + 1;
-        }
-        setTime(newTime);
-    };
+    // useEffect(() => {
+    //     setCount(1);
+    // }, [count])
 
+    // const [count, setCount] = useState(0);
+    // console.log("State", count)
+
+    // useEffect(() => {
+    //     setCount(1);
+    //     return () => {
+    //         console.log("State", count)
+    //     }//cleanup Function
+    // }, [count])
+
+    // const [count, setCount] = useState(0);
+    // console.log("State", count)
+
+    // useEffect(() => {
+    //     setCount(1);//set함수는 비동기
+    //     console.log("State", count)
+    //     setCount(2);
+    //     console.log("State", count)
+    //     setCount(3);
+    //     console.log("State", count)
+    // }, [count])
+
+    // const [count, setCount] = useState(0);
+    // console.log("State", count)
+
+    // useEffect(() => {
+    //     setCount(prev => prev + 1)
+    //     setCount(prev => prev + 1)
+    //     setCount(prev => prev + 1)
+    // }, [])
+
+    const [count, setCount] = useState(0);
+    const [value, setValue] = useState(0);
+
+    console.log("Value", value)
+    useEffect(() => {
+        setCount(prev => prev + 3)
+        setCount(prev => prev + 3)
+        setValue(count)
+    }, [])
     return (
         <div>
-            <span>{time}</span>
-            <button onClick={handleClick}>Update</button>
+            <span>{count}</span>
         </div>
     );
 };
